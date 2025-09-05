d5ec9d61628534cc6624659b19279931c7a544dc
PrataMatheus <102768873+PrataMatheus@users.noreply.github.com>
2025-09-05 00:15:59 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -25,7 +25,6 @@ function iniciar(){
     if (timerId !== null) {
         clearInterval(timerId)
 
-        timer = tempo_trabalho
     }
 
     timerId = setInterval(tick, 1000);
@@ -35,30 +34,48 @@ function iniciar(){
 function pausar() {
     if (timerId != null) {
         
+        clearInterval(timerId)
+        timerId = null
     }
 
-    clearInterval(timerId)
-    timerId = null
 }
 
-
-
-
     function tick(){
+
+
+
         let timerDiv = document.getElementById('timerDiv')
-        timerDiv.textContent=timer; 
+        
         console.log(timer)
         timer = timer - 1 
+        display()
+
         if (timer <= 0) {
             if (fase == 'trabalho') {
                 fase = 'descanso'
                 timer = tempo_descanco
             console.log('mudou para descanso') 
-            }else if(fase == 'descanso'){
+            }else{
                 fase = 'trabalho'
                 timer = tempo_trabalho
                 console.log('mudou para trabalho') 
             }
         }
+
     }
 
+function display() {
+    let minutos = Math.floor(timer / 60)
+    let segundos = timer % 60
+    if (segundos < 10) segundos = '0' + segundos
+
+    let timerDiv = document.getElementById('timerDiv')
+    timerDiv.textContent=minutos + ':' + segundos 
+    
+}
+
+
+
+window.addEventListener('load', () => {
+    display();
+});
